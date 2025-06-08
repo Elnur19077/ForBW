052b07403cf77f7ef363ba7fbc060b0431dc430d
Elnur19077 <elnurquliyev673@gmail.com>
2025-06-09 01:16:07 +0400

    gg

--- src/main/java/bw/black/confiq/CustomLocalDateTimeDeserializer.java
+++ src/main/java/bw/black/confiq/CustomLocalDateTimeDeserializer.java
@@ -10,13 +10,12 @@ import java.util.Locale;
 
 public class CustomLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
 
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yy HH.mm.ss.nnnnnnnnn", Locale.ENGLISH);
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("dd-MMM-yy HH.mm.ss.SSSSSSSSS", Locale.ENGLISH);
 
     @Override
     public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        String date = p.getText();
-        // Saat hissəsindəki nöqtələri ':' ilə dəyişirik
-        date = date.replace('.', ':');
+        String date = p.getText().trim();
         return LocalDateTime.parse(date, formatter);
     }
 }
