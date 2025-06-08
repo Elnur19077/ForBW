b9b7d1de6eb4960bc6f5bc75663fe55214242f23
Elnur19077 <elnurquliyev673@gmail.com>
2025-06-09 01:26:25 +0400

    jh

--- src/main/java/bw/black/entity/HRR.java
+++ src/main/java/bw/black/entity/HRR.java
@@ -3,37 +3,36 @@ package bw.black.entity;
 import bw.black.confiq.CustomLocalDateTimeDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.DynamicInsert;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.sql.Time;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "HRR")
-@Setter
-@Getter
-@DynamicInsert
-@Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class HRR {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "HRR_seq")
-    @SequenceGenerator(name = "hrr_seq", sequenceName = "HRR_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String personId;
-    private String Name;
-    private String Department;
+    private String name;
+    private String department;
+
+    @Column(name = "time", columnDefinition = "TIMESTAMP")
+    @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
+    private LocalDateTime time;
+
+
     private String attendanceStatus;
     private String attendanceCheckPoint;
     private String customName;
     private String dataSource;
-    private String HandlingType;
-    private String Temperature;
-    private String Abnormal;
-    @Column(name = "time")
-    @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
-    private LocalDateTime time;
+    private String handlingType;
+    private String temperature;
+    private String abnormal;
 }
