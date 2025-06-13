ba6ffdb62aa09d100760e7737c061b1c16075618
Elnur19077 <elnurquliyev673@gmail.com>
2025-06-13 11:21:00 +0400

    last uptade 13.06.2025 11:20

--- src/main/java/bw/black/BlackApplication.java
+++ src/main/java/bw/black/BlackApplication.java
@@ -1,10 +1,7 @@
 package bw.black;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class BlackApplication {
@@ -13,8 +10,5 @@ public class BlackApplication {
         SpringApplication.run(BlackApplication.class, args);
     }
 
-    @PostConstruct
-    public void init(){
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Baku"));
-    }
+
 }
