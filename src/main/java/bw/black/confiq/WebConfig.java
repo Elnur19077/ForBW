b36c8b55798d7ff80a1c905b2c02a8871525f74a
Elnur19077 <elnurquliyev673@gmail.com>
2025-06-18 10:40:33 +0400

    last uptade 18.06.2025 1007

--- src/main/java/bw/black/confiq/WebConfig.java
+++ src/main/java/bw/black/confiq/WebConfig.java
@@ -3,6 +3,7 @@ package bw.black.confiq;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +18,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // bu true olduqda allowedOrigins("*") işləməz, amma allowedOriginPatterns("*") işləyir
+
+
     }
-}
+
+
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/uploads/**")
+                    .addResourceLocations("file:uploads/");  // Fiziki uploads qovluğunun yeri
+        }
+    }
+
+
