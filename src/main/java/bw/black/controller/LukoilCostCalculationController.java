c97cf8d16be92478e103436bb90cb3ccb950a6c3
Elnur19077 <elnurquliyev673@gmail.com>
2025-07-10 10:59:16 +0400

    last uptade 10.07.2025 10:59

--- src/main/java/bw/black/controller/LukoilCostCalculationController.java
+++ src/main/java/bw/black/controller/LukoilCostCalculationController.java
@@ -5,6 +5,7 @@ import bw.black.dto.response.LukoilCostCalculationResponse;
 import bw.black.entity.LukoilCostCalculation;
 import bw.black.service.LukoilCostCalculationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,21 +15,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class LukoilCostCalculationController {
     private final LukoilCostCalculationService lukoilCostCalculationService;
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @PostMapping("/calculate")
     public LukoilCostCalculationResponse calculate(@RequestBody LukoilCostCalculationRequest request) {
         return lukoilCostCalculationService.calculate(request);
     }
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @PostMapping("/submit")
     public LukoilCostCalculationResponse submit(@RequestBody LukoilCostCalculationRequest request) {
         return lukoilCostCalculationService.submit(request);
     }
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @GetMapping
     public List<LukoilCostCalculation> getAll() {
         return lukoilCostCalculationService.getAllCalculations();
     }
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
