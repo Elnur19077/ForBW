ba443fc35b1c5bcf3ee23c7322cecb35f5b4cb0f
Elnur19077 <snin_emailin@example.com>
2025-07-28 00:59:30 +0400

    28.07.2025  00:59

--- src/main/java/bw/black/controller/UaeServiceController.java
+++ src/main/java/bw/black/controller/UaeServiceController.java
@@ -4,6 +4,7 @@ import bw.black.dto.request.UaeServiceRequest;
 import bw.black.dto.response.UaeServiceResponse;
 import bw.black.service.UaeServiceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,31 +14,37 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UaeServiceController {
     private final UaeServiceService service;
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @PostMapping("/calculate")
     public UaeServiceResponse calculate(@RequestBody UaeServiceRequest request) {
         return service.calculate(request);
     }
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @PostMapping("/submit")
     public UaeServiceResponse submit(@RequestBody UaeServiceRequest request) {
         return service.submit(request);
     }
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
-    @GetMapping
+    @GetMapping("/all")
     public List<UaeServiceResponse> getAll() {
         return service.findAll();
     }
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @GetMapping("/{id}")
     public UaeServiceResponse getById(@PathVariable Long id) {
         return service.findById(id);
     }
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
     }
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN')or hasAuthority('OPERATOR')")
 
     @PutMapping("/{id}")
     public UaeServiceResponse update(@PathVariable Long id, @RequestBody UaeServiceRequest request) {
